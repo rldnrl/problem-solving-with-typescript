9f51d3792a746350f82568bd6be34244139de50a
rldnrl <devpark1002@gmail.com>
2022-05-03 17:20:53 +0900

    feat: add test case

--- packages/datastructure/src/Queue/Queue.test.ts
+++ packages/datastructure/src/Queue/Queue.test.ts
@@ -4,10 +4,10 @@ describe('Queue', () => {
   it('should create Queue Instance', () => {
     const queue = new Queue<number>(20);
 
-    expect(queue.isEmpty()).toBe(true);
+    expect(queue).toBeInstanceOf(Queue);
   });
 
-  it('should enqueue', () => {
+  it('enqueue test', () => {
     const queue = new Queue<number>(20);
 
     queue.enqueue(3);
@@ -19,7 +19,7 @@ describe('Queue', () => {
     expect(queue.peek()).toBe(3);
   });
 
-  it('should dequeue', () => {
+  it('dequeue test', () => {
     const queue = new Queue<number>(20);
 
     queue.enqueue(3);
@@ -30,5 +30,23 @@ describe('Queue', () => {
 
     expect(queue.dequeue()).toBe(3);
     expect(queue.peek()).toBe(4);
+  });
+
+  it('isFull test', () => {
+    const queue = new Queue<number>(5);
+
+    queue.enqueue(3);
+    queue.enqueue(4);
+    queue.enqueue(5);
+    queue.enqueue(6);
+    queue.enqueue(7);
+
+    expect(queue.isFull()).toBe(true);
+  });
+
+  it('isEmpty test', () => {
+    const queue = new Queue<number>(5);
+
+    expect(queue.isEmpty()).toBe(true);
   });
 });
