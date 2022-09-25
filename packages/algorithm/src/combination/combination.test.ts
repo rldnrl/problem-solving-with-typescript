835752d28d3a11d1542e9555ec487bb9fb6d233b
rldnrl <devpark1002@gmail.com>
2022-09-25 12:50:00 +0900

    test: 테스트 코드 수정

--- packages/algorithm/src/combination/combination.test.ts
+++ packages/algorithm/src/combination/combination.test.ts
@@ -2,8 +2,8 @@ import combination from "./combination";
 
 describe('combination test', function () {
   it('should be success', function () {
-    expect(combination(3, 2)).toStrictEqual([[1, 2], [1, 3], [2, 3]])
-    expect(combination(4, 2)).toStrictEqual([[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]])
-    expect(combination(3, 1)).toStrictEqual([[1], [2], [3]])
+    expect(combination(3, 2)).toStrictEqual([[0, 1], [0, 2], [1, 2]])
+    expect(combination(4, 2)).toStrictEqual([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]])
+    expect(combination(3, 1)).toStrictEqual([[0], [1], [2]])
   });
 });
