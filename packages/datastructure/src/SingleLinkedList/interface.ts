fe5fa260a4dc11430edb49ecc2ca1943b9879f82
rldnrl <devpark1002@gmail.com>
2022-11-12 16:28:29 +0900

    feat: define unshift method

--- packages/datastructure/src/SingleLinkedList/interface.ts
+++ packages/datastructure/src/SingleLinkedList/interface.ts
@@ -1,7 +1,8 @@
-import { LinkedListNode } from './SingleLinkedList';
+import {LinkedListNode} from './SingleLinkedList';
 
 export interface ISingleLinkedList<T> {
   push(val: T): void;
   pop(): LinkedListNode<T> | undefined;
   shift(): LinkedListNode<T> | undefined;
+  unshift(val: T): this;
 }
