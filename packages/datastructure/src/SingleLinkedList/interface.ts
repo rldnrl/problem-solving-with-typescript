232a9431056095f82f82d743669fbf59fe53752b
rldnrl <devpark1002@gmail.com>
2022-11-12 16:05:30 +0900

    feat: add shift method

--- packages/datastructure/src/SingleLinkedList/interface.ts
+++ packages/datastructure/src/SingleLinkedList/interface.ts
@@ -3,4 +3,5 @@ import { LinkedListNode } from './SingleLinkedList';
 export interface ISingleLinkedList<T> {
   push(val: T): void;
   pop(): LinkedListNode<T> | undefined;
+  shift(): LinkedListNode<T> | undefined;
 }
