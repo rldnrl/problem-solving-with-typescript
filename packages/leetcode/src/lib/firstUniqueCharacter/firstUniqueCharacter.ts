25fdb1d1181eeed3a25b230597cee9dde41a0c8f
rldnrl <devpark1002@gmail.com>
2022-12-16 18:45:43 +0900

    feat: set up default value

--- packages/leetcode/src/lib/firstUniqueCharacter/firstUniqueCharacter.ts
+++ packages/leetcode/src/lib/firstUniqueCharacter/firstUniqueCharacter.ts
@@ -2,12 +2,8 @@ export default function firstUniqueCharacter(s: string) {
   const chars = new Map<string, number>();
 
   for (const element of s) {
-    if (!chars.has(element)) {
-      chars.set(element, 1)
-    } else {
-      const count = chars.get(element) || 1
-      chars.set(element, count + 1)
-    }
+    const count = chars.get(element) || 0
+    chars.set(element, count + 1)
   }
 
   for (let i = 0; i < s.length; i++) {
