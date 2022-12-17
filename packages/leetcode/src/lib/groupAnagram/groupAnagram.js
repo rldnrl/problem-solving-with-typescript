7cc5a64a46ac948036d74a349abf60a72b7bf9f8
rldnrl <devpark1002@gmail.com>
2022-12-17 18:12:45 +0900

    feat: implement groupAnagram.js with frequency array

--- packages/leetcode/src/lib/groupAnagram/groupAnagram.js
+++ packages/leetcode/src/lib/groupAnagram/groupAnagram.js
@@ -17,4 +17,28 @@ function groupAnagramWithSort(strs) {
   return [...map.values()]
 }
 
+/**
+ *
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+function groupAnagramWithFrequency(strs) {
+  const map = new Map()
+
+  strs.forEach((str) => {
+    const frequency = Array.from({ length: 26 }).fill(0)
+    for (const c of str) {
+      frequency[c.charCodeAt(0) - 'a'.charCodeAt(0)]++
+    }
+
+    const key = frequency.map((count) => `#${count}`).join("")
+    const values = map.get(key) || []
+    values.push(str)
+    map.set(key, values)
+  })
+
+  return [...map.values()]
+}
+
 console.log(groupAnagramWithSort(["eat","tea","tan","ate","nat","bat"]))
+console.log(groupAnagramWithFrequency(["eat","tea","tan","ate","nat","bat"]))
