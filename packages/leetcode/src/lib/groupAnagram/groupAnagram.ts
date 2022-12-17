062ad4b76b8440b15f3107e20b6617a2a288520d
rldnrl <devpark1002@gmail.com>
2022-12-17 18:12:12 +0900

    feat: add new line

--- packages/leetcode/src/lib/groupAnagram/groupAnagram.ts
+++ packages/leetcode/src/lib/groupAnagram/groupAnagram.ts
@@ -5,9 +5,11 @@
 function groupAnagramWithSorting(strs: string[]): string[][] {
   const map: Map<string, string[]> = strs.reduce((prev, str) => {
     const sortedStr = str.split("").sort().join("")
+
     const values = prev.get(sortedStr) || []
     values.push(str)
     prev.set(sortedStr, values)
+
     return prev
   }, new Map())
 
