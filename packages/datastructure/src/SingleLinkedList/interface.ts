da193e592fd0316dcbbf8e3d80ca2c1478618b85
rldnrl <devpark1002@gmail.com>
2022-12-21 15:19:16 +0900

    feat: change ListNode

--- packages/datastructure/src/SingleLinkedList/interface.ts
+++ packages/datastructure/src/SingleLinkedList/interface.ts
@@ -1,14 +1,14 @@
-import {LinkedListNode} from './SingleLinkedList';
+import {ListNode} from './SingleLinkedList';
 
 export interface ISingleLinkedList<T> {
   push(val: T): void;
-  pop(): LinkedListNode<T> | undefined;
-  shift(): LinkedListNode<T> | undefined;
-  unshift(val: T): this;
-  get(index: number): LinkedListNode<T> | null;
+  pop(): ListNode<T> | undefined;
+  shift(): ListNode<T> | undefined;
+  unshift(val: T): void;
+  get(index: number): ListNode<T> | null;
   set(index: number, val: T): boolean;
   insert(index: number, val: T): boolean;
-  remove(index: number): LinkedListNode<T> | undefined;
+  remove(index: number): ListNode<T> | undefined;
   reverse(): void;
   print(): void;
 }
