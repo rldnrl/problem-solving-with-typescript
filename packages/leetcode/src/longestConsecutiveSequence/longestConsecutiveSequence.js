3ae0e1573d6571361f97a9fd78b50437a8e3a883
rldnrl <devpark1002@gmail.com>
2023-01-19 21:06:27 +0900

    feat: implement longest consecutive with sort

--- packages/leetcode/src/longestConsecutiveSequence/longestConsecutiveSequence.js
+++ packages/leetcode/src/longestConsecutiveSequence/longestConsecutiveSequence.js
@@ -19,4 +19,31 @@ function longestConsecutive(nums) {
   return longest;
 }
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function longestConsecutiveWithSort(nums) {
+  if (nums.length === 0) return 0;
+
+  nums.sort((a, b) => a - b);
+
+  let answer = 0;
+  let length = 0;
+  const set = new Set(nums);
+  const uniqueNums = [...set]
+
+  for(let i = 1; i < uniqueNums.length - 1; i++) {
+    if (uniqueNums[i] - uniqueNums[i - 1] === 1) {
+      length++;
+    } else {
+      answer = Math.max(answer, length + 1);
+      length = 0;
+    }
+  }
+
+  return Math.max(answer, length + 1);
+}
+
 console.log(longestConsecutive([100,4,200,1,3,2]));
+console.log(longestConsecutiveWithSort([100,4,200,1,3,2]));
