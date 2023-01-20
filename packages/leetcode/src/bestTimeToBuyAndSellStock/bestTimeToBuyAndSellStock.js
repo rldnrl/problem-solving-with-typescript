84c8c9543476d86c852e7b7398b76691bf1cf3ac
rldnrl <devpark1002@gmail.com>
2023-01-20 16:52:10 +0900

    feat: solve best time to buy and sell stock with sliding window

--- packages/leetcode/src/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.js
+++ packages/leetcode/src/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.js
@@ -16,3 +16,24 @@ function maxProfit(prices) {
 
   return answer;
 }
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ function maxProfitWithSlidingWindow(prices) {
+  let answer = 0;
+  
+  let left = 0;
+  for (let right = 1; right < prices.length; right++) {
+    if (prices[left] < prices[right]) {
+      answer = Math.max(answer, prices[right] - prices[left])
+    } else {
+      left = right;
+    }
+  }
+
+  return answer;
+}
+
+console.log(maxProfitWithSlidingWindow([7,1,5,3,6,4]));
